22574de42b25d4cd058a48138801669bd5400c64
kannna5296 <kannnahacks5296@gmail.com>
2023-10-28 14:13:09 +0900

    売り切れのやつは出さない

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const items = ref([
       "刻んだ野菜をアボカドと混ぜてディップに。こんがり焼いたバゲットとお召し上がりください。",
     price: 480,
     image: "/images/item1.jpg",
+    soldOut: false,
   },
   {
     id: 2,
@@ -17,6 +18,7 @@ const items = ref([
       "子供のころに食べたかった、あのホットケーキを再現しました。素朴でどこか懐かしい味をどうぞ。",
     price: 1180,
     image: "/images/item2.jpg",
+    soldOut: false,
   },
   {
     id: 3,
@@ -25,6 +27,7 @@ const items = ref([
       "ロサンゼルス生まれのスパークリングウォーター。ノンカロリー、ノンアルコールの新感覚飲料です。",
     price: 320,
     image: "/images/item3.jpg",
+    soldOut: true,
   },
   {
     id: 4,
@@ -33,6 +36,7 @@ const items = ref([
       "イタリア産チーズをたっぷりかけたアツアツのフレンチフライ。みんな大好きな一品です。",
     price: 670,
     image: "/images/item4.jpg",
+    soldOut: false,
   },
 ]);
 </script>
@@ -44,7 +48,7 @@ const items = ref([
   </header>
   <main class="main">
     <template v-for="(item, index) in items" :key="item.id">
-      <div class="item">
+      <div v-if="!item.soldOut" class="item">
         <div class="thumbnail">
           <img :src="item.image" alt="" />
         </div>
